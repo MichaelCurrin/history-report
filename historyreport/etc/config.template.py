50a337352129729fdaaeadce51bd4ee1f36d3fb2
MichaelCurrin <michael.currin@gmail.com>
2018-12-17 01:18:55 +0200

    feature: Create config.template.py

---
+++ historyreport/etc/config.template.py
@@ -0,0 +1,20 @@
+"""
+Config module.
+"""
+import os
+
+
+APP_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+VAR_DIR = os.path.join(APP_DIR, 'var')
+
+JSON_HISTORY_PATH = os.path.join(VAR_DIR, 'BrowserHistory.json')
+CSV_REPORT_PATH = os.path.join(VAR_DIR, 'report.csv')
+
+
+IGNORE_DOMAINS = frozenset((
+    'm.facebook.com',
+    'facebook.com',
+    'www.google.com',
+    'www.youtube.com',
+    'www.instagram',
+))
